428fa4a2cb39b2be6941aa33e374446468f5cf06
LongB40 <53593436+LongB40@users.noreply.github.com>
2021-12-21 23:33:38 +1100

    commenting and cleaning up

--- Spotify Automated Mailing/main.py
+++ Spotify Automated Mailing/main.py
@@ -6,31 +6,56 @@ import codecs
 
 
 def main():
+    """
+    This is our main function, where it will handles most of the logistic of the system.
+    TODO: 
+    - Automated the whole thing.
+    """
+
+    # Initialize our bot to store the main user email information and the list of recievers.
     bot = User()
+
+    # Initialize the object for structure our email.
     email = EmailMessage()
+
+    # Initialize our connection with gmail.com server
     gmail_server = smtplib.SMTP('smtp.gmail.com', 587)
     gmail_server.starttls()
     gmail_server.login(bot.EMAIL_ADDRESS, bot.EMAIL_PASSWORD)
 
+    # Retrieve list of recipients from our bot
     receiver_list = json.loads(bot.getList())
 
+    # Go through each reciepents and send them an email.
     for key in receiver_list:
         email['from'] = bot.EMAIL_ADDRESS
         email['to'] = receiver_list[key]
-        email['subject'] = "This is a reminder for you to pay, Long!"
+        email['subject'] = "This is a reminder for you to pay Long!"
         email.set_content(process_main_content('content.txt', key))
         gmail_server.send_message(email)
     
+    # Clean up and notification.
     gmail_server.quit()
 
     print ("Your mail has been sent successfully")
 
 
 def process_main_content(filename, recipient_name):
+    """
+    This function is for processing the main content of the email.
+        @Filename: The name of our content file
+        @recipient_name: who is the one will be reciving this message
+
+    TODO: 
+    - Adding a generated joke or memes
+    """
+    
+    # Open content file with utf-8 encoded
     with codecs.open(filename, 'r', "utf-8") as file:
         message = file.read()
 
-    message = message.replace("@", recipient_name)
+    # Replace @ with the recipent name
+    message = message.replace("@", recipient_name) 
 
     file.close()
 
