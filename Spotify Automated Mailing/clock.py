6391265dbbf1f84bf9f8d9758f71896cd0a1348c
LongB40 <dinhbaolongvn@gmail.com>
2022-01-11 14:44:34 +1100

    adding a clock to the main program

---
+++ Spotify Automated Mailing/clock.py
@@ -0,0 +1,19 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from main import main
+
+# Create a blocking scheduler object.
+scheduler = BlockingScheduler()
+
+def scheduled_job():
+    """
+    This function is for scheduling a job, from the function of main.py
+    """
+    main()
+    print ("Your mail has been sent successfully")
+
+# Add a job that will start the scheduled_job function on the given time. for more information
+# please look at https://apscheduler.readthedocs.io/en/3.x/index.html
+scheduler.add_job(scheduled_job, 'cron', year='*', month='*', day=19, hour=12)
+
+# Start the scheduler object.
+scheduler.start()
